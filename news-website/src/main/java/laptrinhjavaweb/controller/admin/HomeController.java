4f54854dd739ebfb0c7365ed7af03f8a5e5a8769
TruongGiangLe <truonggiangle17998@gmail.com>
2020-08-11 22:59:57 +0700

    create project

---
+++ news-website/src/main/java/laptrinhjavaweb/controller/admin/HomeController.java
@@ -0,0 +1,27 @@
+package laptrinhjavaweb.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns =  {"/admin-home"})
+public class HomeController extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse reponse) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse reponse) throws ServletException, IOException {
+		
+	}
+	
+}
