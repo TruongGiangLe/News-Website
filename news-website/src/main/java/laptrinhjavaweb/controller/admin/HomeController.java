41e513e9d65a1740af24b1d66da6656095c0e57e
TruongGiangLe <truonggiangle17998@gmail.com>
2020-08-12 22:17:37 +0700

    design layout

--- news-website/src/main/java/laptrinhjavaweb/controller/admin/HomeController.java
+++ news-website/src/main/java/laptrinhjavaweb/controller/admin/HomeController.java
@@ -2,6 +2,7 @@ package laptrinhjavaweb.controller.admin;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +17,9 @@ public class HomeController extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse reponse) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/views/admin/home.jsp");
+		rd.forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse reponse) throws ServletException, IOException {
--- news-website/src/main/java/laptrinhjavaweb/controller/web/HomeController.java
+++ news-website/src/main/java/laptrinhjavaweb/controller/web/HomeController.java
@@ -20,11 +20,6 @@ public class HomeController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserModel userModel = new UserModel();
-		userModel.setFullName("Hello world");
-		// đẩy dữ liệu ra ngoài views
-		// model là cái dùng lấy dữ liệu ra ở view
-		request.setAttribute("model", userModel);
 		RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
 		rd.forward(request, response);
 	}
